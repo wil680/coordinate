347e9a9a44fa255df8ce7d5e756d8241f55a16ab
William <william.pangh@gmail.com>
2025-09-18 11:02:41 -0400

    Implemented React Bits text type to launch page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 import Link from "next/link"
+import TextType from "@/components/ui/texttype"
 
 export default function Home() {
   return (
-    <main className="min-h-screen grid place-items-center p-16">
+    <main className="min-h-screen grid items-start justify-center p-16 bg-blue-50">
       <div className="text-center space-y-3">
-        <h1 className="text-4xl font-bold">Schedule Your Meetings</h1>
+        <h1 className="text-4xl font-bold">
+          <TextType
+            text={["Welcome to Coordinate", "Your Personal Calendar App"]}
+            typingSpeed={70}
+            deletingSpeed={40}
+            pauseDuration={1500}
+            loop
+            className="inline-block"
+            textColors={["text-blue-600", "text-green-600"]}
+          />
+        </h1>
         <div className="flex gap-4 justify-center">
           <Link className="underline" href="/api/auth/signin?callbackUrl=/dashboard">Sign in</Link>
           <Link className="underline" href="/api/auth/signout">Sign out</Link>
@@ -13,3 +24,4 @@ export default function Home() {
     </main>
   )
 }
+
--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,6 +1,6 @@
 function Navbar(){
 return(
-<div className="navbar bg-grey-100 ">
+<div className="navbar bg-blue-50 ">
   <div className="flex-1">
     <a className="btn btn-ghost text-xl">Coordinate</a>
   </div>
